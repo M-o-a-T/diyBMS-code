1b9b6b1ba82721daca2f3f1785e3e92d34453e88
Matthias Urlichs <matthias@urlichs.de>
2022-04-07 17:13:30 +0200

    Pythonize the versioning buildscript
    Also, use the FIRST eight hex digits of the hash, not the last.
    We want to be able to actually identify the commit with git …

--- ATTINYCellModule/buildscript_versioning.py
+++ ATTINYCellModule/buildscript_versioning.py
@@ -8,23 +8,21 @@ Import("env")
 
 git_sha=None
 
-AreWeInGitHubAction = True if "GITHUB_SHA" in env else False
-
-if (AreWeInGitHubAction):
+try:
     git_sha=env["GITHUB_SHA"]
-else:
+except KeyError:
     if (path.exists('..'+os.path.sep+'.git')):
         # Get the latest GIT version header/name
         try:
             git_sha = subprocess.check_output(['git','log','-1','--pretty=format:%H']).decode('utf-8')
-        except:
+        except Exception:
             # Ignore any error, user may not have GIT installed
             git_sha = None
 
 # print(env.Dump())
 
 env.Append(git_sha=git_sha)
-env.Append(git_sha_short=git_sha[32:])
+env.Append(git_sha_short=git_sha[0:8])
 
 
 include_dir = os.path.join(env.get('PROJECT_DIR'), 'include')
@@ -34,36 +32,18 @@ if (os.path.exists(include_dir) == False):
 
 
 with open(os.path.join(include_dir, 'EmbeddedFiles_Defines.h'), 'w') as f:
-    f.write("// This is an automatically generated file, any changes will be overwritten on compiliation!\n")
-    f.write("// DO NOT CHECK THIS INTO SOURCE CONTROL\n")
-    f.write("\n\n#ifndef EmbeddedFiles_Defines_H\n#define EmbeddedFiles_Defines_H\n\n")
+    f.write(f"""\
+// This is an automatically generated file, any changes will be overwritten on compiliation!
+// DO NOT CHECK THIS INTO SOURCE CONTROL
 
-    f.write("static const char GIT_VERSION[] = \"")
-    if (git_sha!=None):
-        f.write(git_sha)
-    else:
-        f.write("LocalCompile")
-    f.write("\";\n\n")
+#ifndef EmbeddedFiles_Defines_H
+#define EmbeddedFiles_Defines_H
 
-    f.write("static const uint16_t GIT_VERSION_B1 = 0x")
-    if (git_sha!=None):
-        f.write(git_sha[32:36])
-    else:
-        #Default for local compile
-        f.write("FFFF")
-    f.write(";\n\n")
+static const char GIT_VERSION[] = "{git_sha or 'LocalCompile'}";
 
-    f.write("static const uint16_t GIT_VERSION_B2 = 0x")
-    if (git_sha!=None):
-        f.write(git_sha[36:])
-    else:
-        #Default for local compile
-        f.write("FFFF")
-    f.write(";\n\n")
+static const uint32_t GIT_VERSION_B = 0x{git_sha[0:8] if git_sha else '0'};
 
+static const char COMPILE_DATE_TIME[] = "{datetime.datetime.utcnow().isoformat()[:-3]+'Z'}";
 
-    f.write("static const char COMPILE_DATE_TIME[] = \"")
-    f.write(datetime.datetime.utcnow().isoformat()[:-3]+'Z')
-    f.write("\";\n\n")
-
-    f.write("#endif")
+#endif
+""")
